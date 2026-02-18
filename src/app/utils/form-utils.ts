7b855f7f371f2ca245cb0769e0a7259002880caf
david <davidgallego20@gmail.com>
2026-02-18 10:42:05 +0100

    feat: Add `FormUtils` for reusable reactive form validation and implement new `SwitchesPage` and `RegisterPage` forms.

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,9 +1,22 @@
-import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
+async function sleep(){
+  return new Promise( (resolve) => {
+    setTimeout(() => {
+      resolve(true)
+    },2500);
+  })
+}
 export class FormUtils {
   // Expresiones regulares
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
 
   static getTextError(errors: ValidationErrors) {
+
+    console.log(errors);
+
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -14,6 +27,18 @@ export class FormUtils {
 
         case 'min':
           return `Valor mínimo de ${errors['min'].min}`;
+
+        case 'email':
+          return `El valor ingresado no es un correo electronico`;
+
+          case 'emailTaken':
+          return `El correo electrico ya esta siendo usado por otro usuario`;
+
+          case 'striderTaken':
+          return `El usuario strider no es valido`;
+
+          default:
+            return `Error de validación no controlado ${key}`;
       }
     }
 
@@ -50,9 +75,46 @@ export class FormUtils {
 
     return FormUtils.getTextError(errors);
   }
+static isFielOneEqualFielTwo(field1:string,field2:string){
+    return(formGroup:AbstractControl)=> {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value == field2Value ? null : { passwordsNotEqual :true};
 
 
+    }
+
+  }
+static async checkingServeResponse(
+  control:AbstractControl
+): Promise<ValidationErrors | null>{
+  console.log('validando contra servidor')
+
+  await sleep(); // 2 segundo y medio
+
+  const formValue = control.value;
+
+  if( formValue == 'hola@mundo.com'){
+    return{
+      emailTaken: true,
+    };
+  }
+  return null;
+}
+
+static notStrider(control:AbstractControl): ValidationErrors | null{
+ const formValue = control.value;
+
+ if (formValue == 'strider'){
+  return{
+    striderTaken:true,
+  };
+ }
+  return null
+
+}
 }
 
 
-// FormUtils.isValidField()
+
